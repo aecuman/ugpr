ef20ad99ae30b5c56035f2ba72f19c62a9bd3b47
aecuman <aecuman24@gmail.com>
2016-08-11 13:21:56 +0300

    firstcommit

---
+++ src/ugpr/Models/Values.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ugpr.Models
+{
+    [Table("Values")]
+    public class Value
+    {
+
+        public int Id { get; set; }
+        public string village { get; set; }
+        public Nullable<double> block { get; set; }
+        public string county { get; set; }
+        public Nullable<double> acreage { get; set; }
+        public Nullable<double> fair_value { get; set; }
+        public string user { get; set; }
+        public string tenure { get; set; }
+        public Nullable<System.DateTime> period { get; set; }
+        public string currency { get; set; }
+        public string development { get; set; }
+        public Nullable<double> rateperacre { get; set; }
+    }
+}
